b712595190b07dbf2b1f99ab0c7d236a1d61f2ad
Pelin <74319493+xidilx@users.noreply.github.com>
2021-05-17 13:13:52 +0300

    Rename lab1b.cpp to lab1b.c

---
+++ lab1b.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+int a,b,toplam;
+printf("ilk tam sayýyý giriniz\n");
+scanf("%d",&a);
+printf("ikinci tam sayýyý giriniz\n");
+scanf("%d",&b);
+toplam=a+b;
+printf("bu iki sayýnýn toplamý=%d",toplam);
+return 0;
+}
